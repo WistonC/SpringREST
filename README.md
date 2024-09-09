ff7e0307d0f377b11107fb18a1fc1837cce5c9ee
Tao Z. <Zhuo.Tao.Winston@outlook.com>
2024-09-09 03:16:27 -0400

    Add README.md and code clean up

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring REST Runbook
+This is a runbook for Spring REST API. It is a guide for developers to understand the best practices and guidelines for developing REST APIs using Spring Boot.
+## Steps to start the Application
+1. Clone the repository
+2. Import the project in your IDE
+3. Run the com.tao.App as a Java Application
+4. Send POST request with JSON as body the application at http://localhost:8080
--- src/test/java/org/tao/UserControllerTest.java
+++ src/test/java/org/tao/UserControllerTest.java
@@ -1,8 +1,6 @@
 package org.tao;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 class UserControllerTest {
@@ -11,21 +9,6 @@ class UserControllerTest {
 
     @BeforeAll
     void setUp() throws Exception {
-        this.testClient = WebTestClient.bindToController(new GreetingController())
-            .build();
-    }
-
-    @Test
-    void register() throws Exception {
-        JSONObject json = new JSONObject();
-        json.put("firstName", "John");
-        json.put("lastName", "Dose");
-        json.put("email", "John.on.ca");
-        json.put("password", "password");
-        System.out.println(json.toString());
-        this.testClient.get().uri("/register")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo(json.toString());
+        this.testClient = WebTestClient.bindToController(new GreetingController()).build();
     }
 }
